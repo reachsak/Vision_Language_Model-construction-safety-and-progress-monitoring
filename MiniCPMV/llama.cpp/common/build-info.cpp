f47d48c6a717e690831ab50d83ac8369cee7b1ba
Reachsak Ly <53211721+reachsak@users.noreply.github.com>
2025-01-17 18:03:12 -0500

    updated code
    updated code

---
+++ MiniCPMV/llama.cpp/common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3271;
+char const *LLAMA_COMMIT = "1781edb6";
+char const *LLAMA_COMPILER = "Apple clang version 15.0.0 (clang-1500.1.0.2.5)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin23.0.0";
